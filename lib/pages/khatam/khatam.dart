a22b4b44ff7e9418fb79fa063e4e180ec6eb7f96
eristaufiqhidayat <eristaufiq@gmail.com>
2025-08-26 07:01:44 +0700

    26 Agustus 2025

---
+++ lib/pages/khatam/khatam.dart
@@ -0,0 +1,254 @@
+// ignore_for_file: deprecated_member_use, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+import 'package:hotmul_quran/const/global_const.dart';
+import 'package:hotmul_quran/pages/khatam/khatam_crud.dart';
+import 'package:hotmul_quran/widget/appbar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:hotmul_quran/service/token_services.dart';
+
+class KhatamPage extends StatefulWidget {
+  const KhatamPage({super.key});
+
+  @override
+  State<KhatamPage> createState() => _KhatamPageState();
+}
+
+class _KhatamPageState extends State<KhatamPage> {
+  int currentPage = 1;
+  int lastPage = 1;
+  List<dynamic> anggota = [];
+  bool isLoading = false;
+  TextEditingController searchController = TextEditingController();
+
+  Future<void> fetchData({int page = 1, String? search}) async {
+    setState(() => isLoading = true);
+    final token = await getToken(); // Ambil token dari SharedPreferences
+    final url = Uri.parse(
+      "${GlobalConst.url}/api/v1/daurah?page=$page&search=${search ?? ''}",
+    );
+    final response = await http.get(
+      url,
+      headers: {"Accept": "application/json", "Authorization": "Bearer $token"},
+    );
+    print("response status code ${response.statusCode}");
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+      print(result);
+      setState(() {
+        anggota = result['data'];
+        currentPage = result['current_page'];
+        lastPage = result['last_page'];
+      });
+    }
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Widget buildPagination() {
+    List<Widget> pages = [];
+
+    for (int i = 1; i <= lastPage; i++) {
+      if (i == 1 ||
+          i == lastPage ||
+          (i >= currentPage - 2 && i <= currentPage + 2)) {
+        pages.add(
+          InkWell(
+            onTap: () => fetchData(page: i),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: i == currentPage ? Colors.blue : Colors.white,
+                borderRadius: BorderRadius.circular(6),
+                border: Border.all(color: Colors.grey),
+              ),
+              child: Text(
+                "$i",
+                style: TextStyle(
+                  color: i == currentPage ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+          ),
+        );
+      } else if (i == currentPage - 3 || i == currentPage + 3) {
+        pages.add(const Text("..."));
+      }
+    }
+
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: pages);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PrimaryAppBar(title: "Daurah"),
+      body: Column(
+        children: [
+          // Tombol Refresh + Add
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.refresh, color: Colors.black),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>((
+                      Set<MaterialState> states,
+                    ) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.red; // saat ditekan
+                      } else if (states.contains(MaterialState.hovered)) {
+                        return Colors
+                            .yellow
+                            .shade100; // saat hover (desktop/web)
+                      }
+                      return Colors.yellow.shade100; // default
+                    }),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero, // biar kotak
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add, color: Colors.white),
+                  label: const Text(
+                    "New",
+                    style: TextStyle(
+                      color: Colors.white, // tulisan putih
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>((
+                      Set<MaterialState> states,
+                    ) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.red; // saat ditekan
+                      } else if (states.contains(MaterialState.hovered)) {
+                        return Colors.blue.shade900; // saat hover (desktop/web)
+                      }
+                      return Colors.blue.shade900; // default
+                    }),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero, // biar kotak
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Search
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: SizedBox(
+              height: 40,
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                  filled: true,
+                  fillColor: Colors.grey.withOpacity(
+                    0.1,
+                  ), // background agak opal
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // sudut melengkung
+                    borderSide: const BorderSide(
+                      color: Colors.grey, // warna border
+                      width: 1.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.blue, // warna border saat fokus
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+                onSubmitted: (value) => fetchData(page: 1, search: value),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          // List Data
+          Expanded(
+            child: isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.separated(
+                    itemCount: anggota.length,
+                    itemBuilder: (context, index) {
+                      final item = anggota[index];
+                      return ListTile(
+                        title: Text(item['group_name'] ?? ""),
+                        subtitle: Text("Daurah id : ${item['group_id']}"),
+                        trailing: PopupMenuButton<String>(
+                          icon: const Icon(Icons.more_vert, color: Colors.red),
+                          onSelected: (value) {
+                            if (value == 'edit') {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      EditKhatamPage(anggota: item),
+                                ),
+                              ).then((updated) {
+                                if (updated == true) {
+                                  fetchData(
+                                    page: currentPage,
+                                  ); // refresh list kalau ada update
+                                }
+                              });
+                            }
+                          },
+                          itemBuilder: (context) => [
+                            const PopupMenuItem(
+                              value: 'edit',
+                              child: Row(
+                                children: [
+                                  Icon(Icons.edit, color: Colors.blue),
+                                  SizedBox(width: 8),
+                                  Text("Update"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const Divider(color: Colors.grey, height: 1),
+                  ),
+          ),
+
+          // Pagination
+          Padding(padding: const EdgeInsets.all(8.0), child: buildPagination()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/khatam/khatam_crud.dart
@@ -0,0 +1,176 @@
+// ignore_for_file: use_build_context_synchronously, non_constant_identifier_names, sort_child_properties_last
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:hotmul_quran/const/global_const.dart';
+import 'package:hotmul_quran/service/token_services.dart';
+import 'package:hotmul_quran/widget/appbar.dart';
+import 'package:hotmul_quran/widget/custom_textfile.dart';
+import 'package:http/http.dart' as http;
+
+class EditKhatamPage extends StatefulWidget {
+  final Map<String, dynamic> anggota;
+
+  const EditKhatamPage({super.key, required this.anggota});
+
+  @override
+  State<EditKhatamPage> createState() => _EditKhatamPageState();
+}
+
+class _EditKhatamPageState extends State<EditKhatamPage> {
+  late TextEditingController group_id;
+  late TextEditingController group_name;
+
+  @override
+  void initState() {
+    super.initState();
+    //print(widget.anggota);
+    group_name = TextEditingController(text: widget.anggota['group_name']);
+    group_id = TextEditingController(
+      text: widget.anggota['group_id'].toString(),
+    );
+  }
+
+  Future<void> saveDelete() async {
+    final token = await getToken();
+    final url = Uri.parse("${GlobalConst.url}/api/v1/anggota/${group_id.text}");
+
+    final payload = {"group_id": group_id.text, "group_name": group_name.text};
+
+    final response = await http.delete(
+      url,
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json", // penting
+      },
+      body: jsonEncode(payload), // jadi JSON
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.pop(context, true);
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Gagal update data")));
+    }
+  }
+
+  Future<void> saveEdit() async {
+    final token = await getToken();
+    final url = Uri.parse("${GlobalConst.url}/api/v1/anggota/${group_id.text}");
+
+    final payload = {"group_id": group_id.text, "group_name": group_name.text};
+
+    // ignore: unused_local_variable
+    final response = await http.put(
+      url,
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json", // penting
+      },
+      body: jsonEncode(payload), // jadi JSON
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //print(statusAnggota);
+
+    return Scaffold(
+      appBar: PrimaryAppBar(title: "Edit Anggota"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomTextField(
+              controller: group_id,
+              label: "Daurah ID",
+              icon: Icons.badge,
+            ),
+            const SizedBox(height: 16),
+            CustomTextField(
+              controller: group_name,
+              label: "Nama Daurah",
+              icon: Icons.person,
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    onPressed: saveEdit,
+                    child: const Text(
+                      "Simpan",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ), // biar tulisan putih
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(40), // tinggi tombol
+                      backgroundColor: Colors.blue, // warna background tombol
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          8,
+                        ), // sudut agak melengkung
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final confirm = await showDialog<bool>(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text("Konfirmasi"),
+                            content: const Text(
+                              "Yakin ingin menghapus data ini?",
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, false),
+                                child: const Text("Batal"),
+                              ),
+                              ElevatedButton(
+                                onPressed: () => Navigator.pop(context, true),
+                                child: const Text("Hapus"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+
+                      if (confirm == true) {
+                        saveDelete(); // baru eksekusi hapus kalau user pilih "Hapus"
+                      }
+                    },
+                    child: const Text(
+                      "Hapus",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(40),
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
